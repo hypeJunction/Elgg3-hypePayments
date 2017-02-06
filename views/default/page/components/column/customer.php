5b4f1bd19b2bb0bb5b2cda3454fa15f06cbef075
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2017-02-06 15:18:59 +0100

    fix(history): provide more detailed customer information

--- views/default/page/components/column/customer.php
+++ views/default/page/components/column/customer.php
@@ -7,16 +7,29 @@ if (!$item instanceof TransactionInterface) {
 	return;
 }
 
+$customer_name = '';
+$customer_email = '';
+$customer_url = false;
+
 $customer = $item->getCustomer();
-if (!$customer) {
-	return;
+if ($customer instanceof ElggEntity) {
+	$customer_name = $customer->getDisplayName();
+	$customer_url = $customer->getURL();
+	if ($customer->email) {
+		$customer_email = '[' . $customer_email . ']';
+	}
 }
-if ($customer->guid) {
-	echo elgg_view('output/url', [
-		'text' => $customer->getDisplayName(),
-		'href' => $customer->getURL(),
-	]);
-} else {
-	echo $customer->title;
+
+if (empty($customer_name)) {
+	$customer_name = implode(' ', [$item->first_name, $item->last_name]);
 }
+
+if (empty($customer_email)) {
+	$customer_email = $item->email;
+}
+
+echo elgg_view('output/url', [
+	'text' => $customer_name . $customer_email,
+	'href' => $customer_url,
+]);
 
