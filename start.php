e3a74cde2ae6ab1de5cf359da7fd92b9061d4a42
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2018-07-07 15:39:59 +0200

    fix(css): extend a component instead of the main css

--- start.php
+++ start.php
@@ -17,9 +17,9 @@ return function () {
 		elgg_register_plugin_hook_handler('permissions_check', 'object', [Permissions::class, 'canEdit']);
 		elgg_register_plugin_hook_handler('permissions_check:delete', 'object', [Permissions::class, 'canDelete']);
 
-		elgg_extend_view('elgg.css', 'payments/stylesheet.css');
+		elgg_extend_view('elements/components.css', 'payments/stylesheet.css');
 		elgg_extend_view('admin.css', 'payments/stylesheet.css');
-		elgg_extend_view('elgg.css', 'input/payments/method.css');
+		elgg_extend_view('elements/forms.css', 'input/payments/method.css');
 
 		elgg_register_collection('collection:object:transaction:customer', \hypeJunction\Payments\CustomerTransactionCollection::class);
 
